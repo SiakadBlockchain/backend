31bf6550cbd95c8067a8ec1f5c32e4106d666d01
ShotakanLe <ramenikoze0909@gmail.com>
2026-05-11 00:27:00 +0700

    setup vm

--- databases/connection.py
+++ databases/connection.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_HOST = "localhost"
-DB_NAME = "db_siakad_blockchain"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "db_siakad_blockchain")
 
 engine = create_engine(f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
 SessionLocal = sessionmaker(bind=engine)
